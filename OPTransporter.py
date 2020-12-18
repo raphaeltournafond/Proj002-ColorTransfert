34a24353ee66e71f7d6e44130b93c9dff08bd1c4
Raphaël Tournafond <raphael.tournafond@gmail.com>
2020-12-18 15:01:31 +0100

    Update README

--- OPTransporter.py
+++ OPTransporter.py
@@ -1,14 +1,38 @@
 # Ambroise Decouttere & Raphael Tournafond
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Color transportation module OPTransporter.py
+
+This module is designed for providing a simple to use color transporter.
+By using this module you will be able to transport the color of a source image onto a destination image
+
+This module require at least python 3.7, numpy and opencv:
+
+`pip install numpy`
+`pip install opencv-python`
+"""
+
+# Imports
 import time
 import numpy as np
 import cv2
+
+__author__ = "Ambroise Decouttere and Raphael Tournafond"
+__copyright__ = "Copyright 2020, PROJ002-USMB"
+__credits__ = ["Ambroise Decouttere", "Raphael Tournafond", "Jacques-Olivier Lachaud", "David Coeurjolly"]
+__licence__ = "MIT"
+__version__ = "1.0"
+__maintainer__ = "Ambroise Decouttere and Raphael Tournafond"
+__email__ = "raphael.tournafond@etu.univ-smb.fr"
+__status__ = "Beta"
 
 
 def get_random_direction():
     """
     Generates a random 3D unit vector (direction) with a uniform spherical distribution
     Algo from http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution
-    :return:
+    :return: direction
     """
     phi = np.random.uniform(0, np.pi * 2)
     costheta = np.random.uniform(-1, 1)
@@ -21,6 +45,12 @@ def get_random_direction():
 
 
 def project_on_direction(direction, image):
+    """
+    Project all the pixels of an image on a direction vector
+    :param direction: 3D array
+    :param image: opencv image
+    :return: array of scalar products
+    """
     # (R, G, B) . (Ur, Ug, Ub) = Rur + Gug + Bub
     b, g, r = cv2.split(image)
     b = np.asarray(b).reshape(-1)
@@ -31,6 +61,12 @@ def project_on_direction(direction, image):
 
 
 def sort_and_project(source, destination):
+    """
+    Project two images and sort their scalar product
+    :param source: source image
+    :param destination: destination image
+    :return: two sorted array of scalar product sorted pixel indexes
+    """
     d = get_random_direction()
     s_p = project_on_direction(d, source)
     d_p = project_on_direction(d, destination)
@@ -40,6 +76,15 @@ def sort_and_project(source, destination):
 
 
 def get_new_projection(source, destination, gamma):
+    """
+    Project two images on a random direction and return a matrix containing the
+    color direction of every channel of every pixel of the image.
+    So adding this matrix to a 3 channels image move their color in the direction of the source image colors
+    :param source: source (color) image
+    :param destination: destination (shape) image
+    :param gamma: fraction of the direction to move the pixels, used to let a chance to reorganise the pixels
+    :return: a matrix of the same shape as the destination
+    """
     d = get_random_direction()
     # Project the source and destination images on the random direction
     s_p = project_on_direction(d, source)
@@ -74,22 +119,35 @@ def convertToFloat(image):
 
 
 class OPTransporter:
+    """
+    The transporter class
+    """
 
     LAB = True
     RGB = False
 
+    # Output file basename
     BASENAME = "output"
 
     def __init__(self, source_path, destination_path, use_lab=False, verbose=False):
+        """
+        OPTransporter constructor
+        :param source_path: image path for the source (color) image
+        :param destination_path: image path for the destination (shape) image
+        :param use_lab: boolean defining if the lab color space is used (default : RGB)
+        :param verbose: boolean defining if the verbose mode should be used (default : False)
+        """
         destination = cv2.imread(destination_path, cv2.IMREAD_COLOR)
         self.use_lab = use_lab
         self.verbose = verbose
         if use_lab:
             if self.verbose:
                 print("Using LAB color space")
+            # Converting to uint8 is requested to go from RBG to LAB
             destination = np.uint8(destination)
             self.destination = cv2.cvtColor(destination, cv2.COLOR_BGR2Lab)
         else:
+            # Using float32 increase the computation speed
             self.destination = np.float32(destination)
         source = cv2.imread(source_path, cv2.IMREAD_COLOR)
         if use_lab:
@@ -98,20 +156,34 @@ class OPTransporter:
         else:
             source = np.float32(source)
         h, w, c = self.destination.shape
+        # Resize to the shape of the destination so we can use different shape of images
+        # Also use the linear interpolation to do the resizing the the colors are still coherent with the source
         self.source = cv2.resize(source, (w, h), interpolation=cv2.INTER_LINEAR)
         if self.verbose:
             print("Color image (source) resized to h:" + str(h) + ", w:" + str(w) + ", c:" + str(c))
 
     def save_image(self, image, version):
+        """
+        Used to construct the output path according to the color space and the algorithm version
+        :param image: image to save
+        :param version: version of the algorithm used
+        :return:
+        """
         filename = self.BASENAME
         if self.use_lab:
             filename += "_lab"
         else:
             filename += "_rgb"
         filename += "_v" + str(version) + ".png"
+        # Save the image using opencv
         cv2.imwrite(filename, image)
 
     def opt_transport_v1(self):
+        """
+        Do the color transportation using the association of the sorted scalar products
+        of the source and destination images on a random direction
+        :return: True if no error
+        """
         t0 = time.time()
         if self.source.shape == self.destination.shape:
             h, w, c = self.source.shape
@@ -120,12 +192,14 @@ class OPTransporter:
             for i in range(len(s_i)):
                 s_i_i = s_i[i]
                 d_i_i = d_i[i]
+                # Retrieve the pixels positions in the matrix
                 xd = d_i_i // w
                 yd = d_i_i % w
                 xs = s_i_i // w
                 ys = s_i_i % w
                 output[xd, yd] = self.source[xs, ys]
             if self.use_lab:
+                # If LAB is used the output file needs to be converted to uint8
                 output = cv2.cvtColor(output.astype('uint8'), cv2.COLOR_Lab2BGR)
             self.save_image(output, 1)
             if self.verbose:
@@ -136,13 +210,22 @@ class OPTransporter:
         return False
 
     def opt_transport_v2(self, gamma, iteration):
+        """
+        Sliced OT Color Transfer
+        Do the color transportation by using a projection of colors in direction of the source image
+        :param gamma: The fraction of the direction to use
+        :param iteration: The number of iteration (projection in a random direction)
+        :return: True if no error
+        """
         t0 = time.time()
         if self.source.shape == self.destination.shape:
             output = self.destination.copy()
             for k in range(iteration):
                 if self.verbose:
                     print('\rIteration ' + str(k+1) + " out of " + str(iteration), end='', flush=True)
+                # Get a fraction of the projection of the destination in the source direction
                 diff = get_new_projection(self.source, output, gamma)
+                # Move the destination image towards the source colors in the random destination
                 output = np.add(output, diff)
             if self.use_lab:
                 output = cv2.cvtColor(output.astype('uint8'), cv2.COLOR_Lab2BGR)
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Proj002-ColorTransfert
 
-Raphaël TOURNAFOND & Ambroise DECOUTTERE - CMI info
+Raphaël TOURNAFOND & Ambroise DECOUTTERE - M2 CMI info
 
 ## Description
 
@@ -8,6 +8,42 @@ Color transfer is an image processing application where you want
 to retarget the color histogram of an input image according to the 
 color histogram of a target one.
 
+## Installation
+
+### Requirements
+
+This module is designed for providing a simple to use color transporter.
+By using this module you will be able to transport the color of a source image onto a destination image
+
+This module require at least python 3.7, numpy and opencv:
+
+`pip install numpy`
+
+`pip install opencv-python`
+
+### Module configuration
+
+Then you need to get the file ```OPTransporter.py``` and put it in your project.
+
+Then you can import it where you need it :
+```python
+from OPTransporter import OPTransporter
+```
+
+Finally you need to instantiate an OPTransporter object.
+For example :
+```py
+ot = OPTransporter("pexelA-0.png", "pexelB-0.png", OPTransporter.RGB, True)
+````
+
+OPTransporter constructor
+
+| Parameter  | Description  | 
+|---|---|
+| source_path | image path for the source (color) image |
+| destination_path | image path for the destination (shape) image |
+| use_lab | boolean defining if the lab color space is used (default : RGB) |
+| verbose | boolean defining if the verbose mode should be used (default : False) |
 
 ## Work done
 
@@ -30,56 +66,61 @@ Size of image used: 1024x1280
 
 ### opt_transport_v1 RGB
 
+```py
+ot = OPTransporter("pexelA-0.png", "pexelB-0.png", OPTransporter.RGB, True)
+ot.opt_transport_v1()
+```
+
 | Source  | Destination  | Result  | 
 |---|---|---|
 | ![](pexelA-0.png)  | ![](pexelB-0.png)  | ![](output_rgb_v1.png)  |
-Time : 5.1 sec
+**Time : 5.1 sec**
 
 ### opt_transport_v1 Lab
+
+```py
+ot = OPTransporter("pexelA-0.png", "pexelB-0.png", OPTransporter.LAB, True)
+ot.opt_transport_v1()
+```
 
 | Source  | Destination  | Result  | 
 |---|---|---|
 | ![](pexelA-0.png)  | ![](pexelB-0.png)  | ![](output_lab_v1.png)  |
-Time : 2.6 sec
+**Time : 2.6 sec**
 
 ### opt_transport_v2 RGB
+
+```py
+ot = OPTransporter("pexelA-0.png", "pexelB-0.png", OPTransporter.RGB, True)
+ot.opt_transport_v2(0.1, 100)
+```
 
 Iterations = 100 / Fraction of directional vector used = 0.1
 
 | Source  | Destination  | Result  | 
 |---|---|---|
 | ![](pexelA-0.png)  | ![](pexelB-0.png)  | ![](output_rgb_v2.png)  |
-Time : 37.4 sec
+**Time : 37.4 sec**
 
 ### opt_transport_v2 Lab
+
+```py
+ot = OPTransporter("pexelA-0.png", "pexelB-0.png", OPTransporter.LAB, True)
+ot.opt_transport_v2(0.1, 100)
+```
 
 Iterations = 100 / Fraction of directional vector used = 0.1
 
 | Source  | Destination  | Result  | 
 |---|---|---|
 | ![](pexelA-0.png)  | ![](pexelB-0.png)  | ![](output_lab_v2.png)  |
-Time : 43.4 sec
+**Time : 43.4 sec**
 
-## Installation
+Despite the increase of execution time the second version (sliced OPT transport)
+give way better result.
+Also note that using the LAB color space give more closer results to the source colors
+but tends to be a bit slower.
 
-First you need to get the file ```OPTransporter.py``` and put it in your project.
+<hr />
 
-Then you can import it where you need it :
-```python
-from OPTransporter import OPTransporter
-```
-
-Finally you need to instantiate an OPTransporter object.
-For example :
-```py
-ot = OPTransporter("pexelA-0.png", "pexelB-0.png", OPTransporter.RGB, True)
-````
-
-OPTransporter constructor
-
-| Parameter  | Description  | 
-|---|---|
-| source_path | image path for the source (color) image |
-| destination_path | image path for the destination (shape) image |
-| use_lab | boolean defining if the lab color space is used (default : RGB) |
-| verbose | boolean defining if the verbose mode should be used (default : False) |
+Voilà! You know everything to use our module now.
